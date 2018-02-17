88ea4f615d735d871cd956f796d82cbb253629b9
Juan Francisco Ternicien <jternicien@alumnos.uai.cl>
2018-02-16 23:38:44 -0300

    clase 12  Ordenar codigos UNREAL

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -9,37 +9,49 @@
 #include "Grabber.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class BUILDINGESCAPE_API UGrabber : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UGrabber();
 
-protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-private:
-	APlayerController * MyPlayer;
 
-	FVector Posicion;
-	FRotator Rotacion;
+
+private:
+	// How far ahead of the player can we reach in cm
 	float Reach = 100.f;
-	UPhysicsHandleComponent* PhysucsHandle = nullptr;
+
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
 	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
 	void Grab();
+
+	// Called when grab is released
 	void Release();
 
-	void FindPhysicsComponents();
+	// Find (assumed) attached phyics handle
+	void FindPhysicsHandleComponent();
+
+	// Setup (assumed) attached input component
 	void SetupInputComponent();
 
-	const FHitResult GetFirstPhysicsBodyInReach ();
-	
+	// Return hit for first physics body in reach
+	const FHitResult GetFirstPhysicsBodyInReach();
+
+	// Returns current start of reach line
+	FVector GetReachLineStart();
+
+	// Returns current end of reach line
+	FVector GetReachLineEnd();
 };
